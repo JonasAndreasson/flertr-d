3d92e7b5fe98c05b6b2ae3c24252c661b69bb8a4
Jonas Andreasson <jo7334an-s@student.lu.se>
2022-09-09 15:01:16 +0200

    Implemented Train

---
+++ widgets/src/train/simulation/TrainMonitor.java
@@ -0,0 +1,32 @@
+package train.simulation;
+import java.util.HashSet;
+import java.util.Set;
+
+import train.model.Segment;
+public class TrainMonitor {
+	private Set<Segment> busySegments;
+	public TrainMonitor() {
+		busySegments = new HashSet<>();
+	}
+	public synchronized void addBusySegment(Segment s) {
+		
+		busySegments.add(s);
+		s.enter();
+	}
+	public synchronized void removeBusySegment(Segment s) {
+		busySegments.remove(s);
+		notifyAll();
+	}
+	public synchronized boolean isSegmentFree(Segment s) {
+		while (busySegments.contains(s)){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		addBusySegment(s);
+		return true;
+	}
+}
--- widgets/src/train/simulation/TrainSimulation.java
+++ widgets/src/train/simulation/TrainSimulation.java
@@ -1,5 +1,7 @@
 package train.simulation;
 
+import java.util.LinkedList;
+
 import train.model.Route;
 import train.model.Segment;
 import train.view.TrainView;
@@ -9,12 +11,39 @@ public class TrainSimulation {
     public static void main(String[] args) {
 
         TrainView view = new TrainView();
-        
-        Route route = view.loadRoute();
-        
-        Segment first = route.next();
-        
-        first.enter();
+        TrainMonitor tm = new TrainMonitor();
+        TrainThread tt;
+        for (int i = 0; i < 20; i++) {
+        	Route route = view.loadRoute();
+        	 tt = new TrainThread(route, tm);
+        	 tt.start();
+        }
     }
-
+    private static class TrainThread extends Thread {
+    	private LinkedList<Segment> queue;
+    	private Route route;
+    	private Segment head,tail;
+    	private TrainMonitor tm;
+    	private TrainThread(Route route,TrainMonitor tm) {
+            this.route = route;
+            this.queue = new LinkedList<>(); //FIFO queue kinda
+            this.tm = tm;
+    	}
+    	public void run() {
+            for (int i = 0; i < 3; i++) {
+            	Segment seg = route.next();
+            	queue.add(seg);
+            	tm.addBusySegment(seg);
+            }
+    		while(true) {
+    				head = route.next();
+            	if(tm.isSegmentFree(head)) {
+            		queue.addLast(head);
+            		tail = queue.poll();
+            		tail.exit();
+            		tm.removeBusySegment(tail);
+            	}
+            }
+    	}
+    }
 }
