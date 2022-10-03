c2f13287cb6a9c4aa1462021b8922560028959ee
Jonas Andreasson <jo7334an-s@student.lu.se>
2022-10-03 12:03:07 +0200

    Working ActorThread

--- wash/src/actor/ActorThread.java
+++ wash/src/actor/ActorThread.java
@@ -1,23 +1,32 @@
 package actor;
-
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 public class ActorThread<M> extends Thread {
-
+	private LinkedBlockingQueue<M> list;
+	public ActorThread(){
+		list = new LinkedBlockingQueue<>();
+	}
+	
+	
     /** Called by another thread, to send a message to this thread. */
     public void send(M message) {
-        // TODO: implement this method (one or a few lines)
+        try {
+			list.put(message);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     /** Returns the first message in the queue, or blocks if none available. */
     protected M receive() throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
+        return list.take();
     }
     
     /** Returns the first message in the queue, or blocks up to 'timeout'
         milliseconds if none available. Returns null if no message is obtained
         within 'timeout' milliseconds. */
     protected M receiveWithTimeout(long timeout) throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
+    	return list.poll(timeout, TimeUnit.MILLISECONDS);
     }
 }
