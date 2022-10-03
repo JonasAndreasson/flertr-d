0dbd398312a1be093520cf8cc66e509b99c4d5e3
Love Olsson <lo0016ol-s@student.lu.se>
2022-10-03 15:12:38 +0200

    Merge branch 'main' of git@github.com:JonasAndreasson/multithread.git into main

--- wash/src/wash/control/Wash.java
+++ wash/src/wash/control/Wash.java
@@ -4,7 +4,8 @@ import wash.io.WashingIO;
 import wash.simulation.WashingSimulator;
 
 public class Wash {
-
+	static Thread current;
+	
     public static void main(String[] args) throws InterruptedException {
         WashingSimulator sim = new WashingSimulator(Settings.SPEEDUP);
         
@@ -21,7 +22,26 @@ public class Wash {
         while (true) {
             int n = io.awaitButton();
             System.out.println("user selected program " + n);
-
+            switch(n) {
+            case 0:
+            	current.interrupt();
+            	
+            	break;
+            case 1:
+            	current = new WashingProgram1(io,temp,water,spin);
+            	current.start();
+            	break;
+            case 2:
+            	current.start();
+            	break;
+            case 3: 
+            	
+             current = new WashingProgram3(io,temp,water,spin);
+             current.start();
+             break;
+            }
+            
+            
             // TODO:
             // if the user presses buttons 1-3, start a washing program
             // if the user presses button 0, and a program has been started, stop it
--- wash/src/wash/control/WaterController.java
+++ wash/src/wash/control/WaterController.java
@@ -2,17 +2,46 @@ package wash.control;
 
 import actor.ActorThread;
 import wash.io.WashingIO;
+import wash.control.WashingMessage.Order.*;
 
 public class WaterController extends ActorThread<WashingMessage> {
 
-    // TODO: add attributes
+	// TODO: add attributes
+	WashingIO io;
 
-    public WaterController(WashingIO io) {
-        // TODO
-    }
+	public WaterController(WashingIO io) {
+		// TODO
+		this.io = io;
 
-    @Override
-    public void run() {
-        // TODO
-    }
+	}
+
+	@Override
+	public void run() {
+		// TODO
+		while (true) {
+			try {
+				WashingMessage op = receive();
+				switch (op.getOrder()) {
+				case WATER_DRAIN:
+					io.fill(false);
+					io.drain(true);
+
+				case WATER_IDLE:
+					io.drain(false);
+					io.fill(false);
+
+				case WATER_FILL:
+					io.drain(false);
+					io.fill(true);
+
+				default:
+
+				}
+
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
